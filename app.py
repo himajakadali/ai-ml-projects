1e7aa6f0c2b54e861fe2ab743f1fcffca75173f1
himajakadali <155795874+himajakadali@users.noreply.github.com>
2024-01-06 11:44:19 +0530

    create ml model

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import streamlit as st
 
 dataset=pd.read_csv('crop.csv')
 X=dataset.iloc[:,:-1].values
-Y=dataset,iloc[:,-1].values
+Y=dataset.iloc[:,-1].values
 
 X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2)
 
